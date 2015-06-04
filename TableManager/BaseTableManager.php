e63eb867c1a67bf23c9ffba878be18be1facb5c8
Kévin Ziemianski <kevin.ziemianski@gmail.com>
2015-06-04 12:40:37 +0200

    Fix sort order

--- TableManager/BaseTableManager.php
+++ TableManager/BaseTableManager.php
@@ -9,59 +9,59 @@ abstract class BaseTableManager
     const NO_SEARCH = 0;
     const FAST_SEARCH = 1;
     const CUSTOM_SEARCH = 2;
-    
+
     /**
-     * @var Request 
+     * @var Request
      */
     protected $request;
 
     /**
      * name => value (string or array if multiple select)
-     *  
-     * @var array 
+     *
+     * @var array
      */
     protected $customSearch = array();
-    
+
     /**
      * @var string
      */
     protected $fastSearch;
-    
+
     /**
      * NO_SEARCH | FAST_SEARCH | CUSTOM_SEARCH
-     * 
-     * @var intager 
+     *
+     * @var intager
      */
     protected $searchType = self::NO_SEARCH;
 
     /**
      * columnName => sortDirection
-     * 
-     * @var array 
+     *
+     * @var array
      */
     protected $sort = array();
-    
+
     /**
-     * @var integer 
+     * @var integer
      */
     protected $limit;
-    
+
     /**
-     * @var integer 
+     * @var integer
      */
     protected $offset;
 
     abstract public function getTotalCount();
-    
+
     abstract public function getFilteredCount();
-    
+
     abstract public function getRows();
-    
+
     abstract protected function getRowArray($row);
 
     /**
      * Set Request to manager to extract and organize data
-     * 
+     *
      * @param \Symfony\Component\HttpFoundation\Request|array $request
      * @return \Plan\TrainingBundle\DataTable\TrainingTableManager
      */
@@ -70,20 +70,20 @@ abstract class BaseTableManager
         $this->request = $request;
         return $this;
     }
-    
+
     public function extactSearch()
     {
         $extractor = new FilterExtractor();
         $extractor->setRequest($this->request)
                   ->extactSearch();
-        
+
         $this->searchType = $extractor->getSearchType();
         $this->fastSearch = $extractor->getFastSearch();
         $this->customSearch = $extractor->getCustomSearch();
-        
+
         return $this;
     }
-    
+
     /**
      * Extract sort data from request
      */
@@ -91,14 +91,18 @@ abstract class BaseTableManager
     {
         if(null != $this->request->get('iSortCol_0', null) ) {
             // dataTable V1.9
-
             $sortColumnNb = intval($this->request->get('iSortCol_0'));
             $sortColumnName = self::secureString($this->request->get('mDataProp_'.$sortColumnNb));
+
+            $sortOrder = $this->request->get('sSortDir_0');
+            if ('asc' != strtolower($sortOrder) && 'desc' != strtolower($sortOrder)) {
+                $sortOrder = intval($sortOrder);
+            }
 
             $this->sort = array(
                 array(
                     'columnName' => $sortColumnName,
-                    'dir' => intval($this->request->get('sSortDir_0'))
+                    'dir' => $sortOrder
                 )
             );
         } else {
@@ -124,7 +128,7 @@ abstract class BaseTableManager
         $this->limit = intval($this->request->get('length', 0));
         $this->offset = intval($this->request->get('start', 0));
     }
-    
+
     /**
      * @param integer $limit
      * @param integer $offset
@@ -134,13 +138,13 @@ abstract class BaseTableManager
     {
         $this->limit = intval($limit);
         $this->offset = intval($offset);
-        
+
         return $this;
     }
-    
+
     /**
      * Create array formated to be send to dataTable
-     * 
+     *
      * @return array
      */
     public function getJsonResponse()
@@ -148,7 +152,7 @@ abstract class BaseTableManager
         $this->extactSearch();
         $this->extractSort();
         $this->extactLimits();
-        
+
         $json = array(
             'draw' => $this->request->get('draw', 0),
             'recordsTotal' => $this->getTotalCount(),
@@ -162,10 +166,10 @@ abstract class BaseTableManager
         }
         return $json;
     }
-    
+
     /**
      * Possibility to Secure (ensure there is no sql injection)
-     * 
+     *
      * @param type $string
      */
     static public function secureString($string)
